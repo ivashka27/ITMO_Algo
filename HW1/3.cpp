32e294b59f03c1ce20e06f43b97d18df309b773c
ivashka27 <41290137+ivashka27@users.noreply.github.com>
2021-09-11 15:50:35 +0300

    Add files via upload

---
+++ HW1/3.cpp
@@ -0,0 +1,47 @@
+#include<bits/stdc++.h>
+
+#define int long long
+
+using namespace std;
+
+//(i, j): A[i] + B[j] == S
+
+int32_t main() {
+    int n, m, s; cin >> n >> m >> s;
+    vector<int> a(n), b(m);
+    for (auto &e : a) cin >> e;
+    for (auto &e : b) cin >> e;
+    int i = 0, j = m - 1;
+    
+    int ans = 0;
+    
+    while (i < n && j >= 0) {
+        int sum = a[i] + b[j];
+        if (sum == s) {
+            cout << i << " " << j << "\n";
+            return 0;
+        }
+        if (a[i] < b[j]) i++;
+        else j++;
+    }
+    
+    while (i < n) {
+        int sum = a[i] + b[j];
+        if (sum == s) {
+            cout << i << " " << j << "\n";
+            return 0;
+        }
+        i++;
+    }
+    
+    while (j >= 0) {
+        int sum = a[i] + b[j];
+        if (sum == s) {
+            cout << i << " " << j << "\n";
+            return 0;
+        }
+        j--;
+    }
+    
+    return 0;
+}
