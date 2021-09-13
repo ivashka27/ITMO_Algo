e9da3b4e4e693955da8efa0c315d09b08cd9a50a
Ivan Palchenkov <41290137+ivashka27@users.noreply.github.com>
2021-09-13 14:29:30 +0300

    Update 4.cpp

--- HW1/4.cpp
+++ HW1/4.cpp
@@ -12,28 +12,23 @@ int32_t main() {
     for (auto &e : a) cin >> e;
     for (auto &e : b) cin >> e;
     int i = 0, j = 0;
-    
+
     int ans = 0;
-    
+
     while (i < n && j < m) {
         while (a[i] < b[j] && i + 1 < n) i++;
         while (b[j] < a[i] && j + 1 < m) j++;
         if (a[i] == b[j]) {
-            ans++;
-            while (i + 1 < n && j + 1 < m && a[i + 1] == b[j + 1]) { 
-                i++, j++, ans++;
-            }
-            while (i + 1 < n && a[i + 1] == b[j]) {                
-                i++, ans++;
-            }
-            while (j + 1 < m && a[i] == b[j + 1]) {
-                j++, ans++;
-            }
-            i++, j++;
+            int cnt1 = 1, cnt2 = 1;
+            while (i + 1 < n && a[i + 1] == a[i]) cnt1++, i++;
+            while (j + 1 < m && b[j + 1] == b[j]) cnt2++, j++;
+            ans += cnt1 * cnt2;
+
         }
+        i++, j++;
     }
-    
+
     cout << ans;
-    
+
     return 0;
 }
